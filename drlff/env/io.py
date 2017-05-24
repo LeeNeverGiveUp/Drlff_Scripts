256f0d69f2aec4be4c3982de8779463708f83018
徐承成 <the0demiurge@gmail.com>
2017-05-24 22:15:30 +0800

    parser block separter done

--- drlff/env/io.py
+++ drlff/env/io.py
@@ -1,29 +1,56 @@
+import pdb
+import re
 # part, block, num
+
+
 class ffield(object):
+
     def __init__(self, path):
         self.path = path
-        self.sep = '^\s*\d+\s*(?:!.*)?$'
+        self.nothead = '\s*-?\d+'
+        self.sep = '^\s*-?\d+\s*(?:!.*)?$'
         self.annotation = '\s*!.*'
 
+        self.parser()
+
     def parser(self):
+
         self.parsed = {
-                'head' = [],
-                'data' = [], # part, block, num
-                'separter' = [],
-                'annotation' = []
-                }
+            'head': [],
+            'data': [],  # part, block, num
+            'separter': [],
+            'annotation': []
+        }
 
         with open(self.path, 'r') as f:
             data = f.readlines()
 
-        i = 0
-        while i < len(data):
-            if re.match(self.annotation, data[i]): # annotation
-                i += 1
+        ishead = True
+        for i in data:
+            if ishead and not re.match(self.nothead, i):
+                self.parsed['head'].append(i)
+            else:
+                ishead = False
+                if re.match(self.annotation, i):
+                    self.parsed['annotation'].append[i]
+                else:
+                    if re.match(self.sep, i):
+                        self.parsed['separter'].append[i]
+                        self.parsed['data'].append([])
+                    else:
+                        if len(self.parsed['data'][-1]) == 0 and not re.match('\s*-?\d|\s*[A-Z][a-z]?\s+-?\d+\.\d+', i):
+                            self.parsed['separter'][-1].append(i)
+                        else:
+                            self.parsed['data'][-1].append(i)
 
-            elif re.match(self.sep, data[i]): # separter found, block seped
-                pass
+
+def main():
+    import conf
+
+f = ffield('ffield')
 
 
-
-
+if __name__ == '__main__':
+    main()
+)
+)
