b26393cda340f59d480a71f80ac998de7bf80402
Charles Xu <charl3s.xu@gmail.com>
2017-06-20 22:47:20 +0800

    parsing done

--- drlff/env/io.py
+++ drlff/env/io.py
@@ -4,6 +4,10 @@ import re
 
 
 class ffield(object):
+    """ Read, Write, ffield file
+    usage:
+        ff = ffield(path)
+    """
 
     def __init__(self, path):
         self.path = path
@@ -14,7 +18,7 @@ class ffield(object):
         self.parser()
 
     def parser(self):
-
+        # read and separate data into parts
         self.parsed = {
             'head': [],
             'data': [],  # part, block, num
@@ -43,3 +47,50 @@ class ffield(object):
                         else:
                             self.parsed['data'][-1].append(line.strip().split())
 
+        # clean comments from parsed data
+        data_cleaned = list()
+        for block in self.parsed['data']:   # every large data block
+            data_cleaned.append(list())
+            for line in block:      # every line
+                data_cleaned[-1].append(list())
+                found_comment = False
+                for element in line:
+                    if not found_comment and element.find('!') == -1:
+                        data_cleaned[-1][-1].append(element)
+                    elif not found_comment and element.find('!') == 0:
+                        found_comment = True
+                    elif not found_comment:
+                        tmp = element[:element.find('!')]
+                        if tmp:
+                            data_cleaned[-1][-1].append(tmp)
+        self.parsed['data'] = data_cleaned
+
+        # split data into small blocks
+        seped = list()
+        for block in self.parsed['data']:
+            if len(block) > 0:
+                seped.append(list())
+                linear = True if block[0][0].find('.') >= 0 else False
+                if linear:      # One line in ffield file is one real logic line
+                    for line in block:
+                        seped[-1].append([line])
+                else:
+                    for line in block:
+                        if line[0].find('.') == -1:
+                            seped[-1].append(list())
+                        seped[-1][-1].append(line)
+        self.parsed['data'] = seped
+
+
+
+
+
+def main():
+    from drlff.env.io import ffield
+    from drlff.conf import files_input
+
+    data = ffield(files_input['ffield'])
+
+
+if __name__ == '__main__':
+    main()
