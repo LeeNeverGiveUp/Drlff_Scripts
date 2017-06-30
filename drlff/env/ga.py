117bf8cea2bcfebf9a2b82354eb12c7bf7cbcf19
徐承成 <the0demiurge@yahoo.com>
2017-06-30 16:19:28 +0800

    step done

--- drlff/env/ga.py
+++ drlff/env/ga.py
@@ -1,23 +1,27 @@
 import os
 import subprocess
-from drlff.conf import env, files_input, files_output
+from drlff.conf import env, files_input, files_output, example_input_path
 
-def step():
+def run():
     if not os.path.isdir(files_output['log']):
         if os.path.exists(files_output['log']):
             os.remove(files_output['log'])
         os.mkdir(files_output['log'])
-    os.chdir(files_output['log'])
 
+    os.chdir(example_input_path)
     pipe = subprocess.Popen(
         [
             'garffield',
-            files_input['geo'],
-            files_input['ffield'],
-            files_input['trainset'],
-            files_input['params'],
-            '-t 1',
-            '-p 2'],
+            'geo.new',
+            'ffield',
+            'trainset.in.new',
+            'params',
+            #files_input['geo'],
+            #files_input['ffield'],
+            #files_input['trainset'],
+            #files_input['params'],
+            '-t','1',
+            '-p','2'],
         env=env,
         stdout=subprocess.PIPE
     )
