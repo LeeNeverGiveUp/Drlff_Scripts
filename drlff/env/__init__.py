9a8ab37e4a4697440d40a489d57bf9ea67379b2e
Charles Xu <charl3s.xu@gmail.com>
2017-07-01 12:03:04 +0800

    完善了一点点

--- drlff/env/__init__.py
+++ drlff/env/__init__.py
@@ -1,20 +1,31 @@
+"""ga envirenment, showes the state, reward, alive info to a deep reinforcement agent.
+Functions:
+    step(act) -> state, reward, alive
+    reset() -> state, reward, alive
+
+Attr:
+    alive: if it's failed, if not fail, alive == True.
+    action_space
+    observation_space
+"""
 from drlff.env import ga, io
 import numpy as np
 
 ffp = io.main()
 observation_space = ffp.range
-action_space = ffp.scale
+scale_space = ffp.scale
+action_space = tuple(range(-len(scale_space), len(scale_space)+1))
 
 
 def act_parser(act):
     # [-len, len]
     act = int(act)
-    if abs(act) > len(action_space):
-        raise ValueError('act out of range of %d' % len(action_space))
-    action = np.zeros(len(action_space))
+    if abs(act) > len(scale_space):
+        raise ValueError('act out of range of %d' % len(scale_space))
+    action = np.zeros(len(scale_space))
     if act != 0:
-        action[abs(act) -1] = sign(act)
-    return action * action_space
+        action[abs(act) - 1] = sign(act)
+    return action * scale_space
 
 
 def step(act):
